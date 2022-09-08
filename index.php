f081c3bd01756a4e9ce76e0882e846bac3b03957
paulosergioduff <paulosergioduff@gmail.com>
2022-09-08 10:04:06 -0300

    Limpando categorias de area de produtos (aguardando ISSUE categorias)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         $page = ob_get_contents();
     ob_end_clean();
 
-    $template = file_get_contents('src/view/categoria.html');
+    $template = file_get_contents('src/view/home.html');
     $template = str_replace('{{content}}', $page, $template);
     
     echo $template;
