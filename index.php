0078c4f7cda23948ac6d9c845bf4203855e3a9ab
paulosergioduff <paulosergioduff@gmail.com>
2022-09-08 15:28:07 -0300

    Funcionalidades mergiadas

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    require_once 'src/controller/HomeCategoryController.php';
-    require_once 'src/controller/CategoryController.php';
+    require_once 'src/controller/HomeController.php';
+    require_once 'src/controller/ProductController.php';
     require_once 'src/controller/ErroController.php';
 
     ob_start();
@@ -9,7 +9,7 @@
         $page = ob_get_contents();
     ob_end_clean();
 
-    $template = file_get_contents('src/view/categoria.html');
+    $template = file_get_contents('src/view/home.html');
     $template = str_replace('{{content}}', $page, $template);
     
     echo $template;
