0591312964d46dcd5f756ac8043d87e643e50299
paulosergioduff <paulosergioduff@gmail.com>
2022-09-10 20:31:16 -0300

    Ajustando composer.json

--- webjump.sql
+++ webjump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Set-2022 às 00:51
+-- Tempo de geração: 11-Set-2022 às 01:16
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.29
 
@@ -32,7 +32,7 @@ USE `webjump`;
 CREATE TABLE `categoria` (
   `id` int(16) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `codigo` bigint(128) NOT NULL
+  `codigo` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -40,8 +40,8 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`id`, `name`, `codigo`) VALUES
-(1, 'eletronicos', 1),
-(2, 'lar', 2);
+(1, 'eletronicos', '1'),
+(2, 'lar', '2');
 
 -- --------------------------------------------------------
 
@@ -56,7 +56,7 @@ CREATE TABLE `produtos` (
   `description` varchar(255) DEFAULT NULL,
   `categoria` varchar(255) DEFAULT NULL,
   `quantidade` int(64) DEFAULT NULL,
-  `codigo` bigint(128) DEFAULT NULL
+  `codigo` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -64,10 +64,10 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `name`, `price`, `description`, `categoria`, `quantidade`, `codigo`) VALUES
-(20, 'Controle Xbox ONE', 450, 'Muito bom', '1', 12, 9223372036854775807),
-(21, 'Teclado Mecânico', 150, 'Muito requisitado', '1', 15, 9223372036854775807),
-(22, 'Playstation5', 4000, 'Grande demanda', '1', 12, 9223372036854775807),
-(24, 'Caneca Star Wars', 40, 'Item raro', '2', 10, 9223372036854775807);
+(20, 'Controle Xbox ONE-A', 450, 'Muito bom', '1', 12, '8223372036854775807'),
+(21, 'Teclado Mecânico', 149, 'Muito requisitado', '1', 15, '1312318239018230912'),
+(22, 'Playstation5', 4000, 'Grande demanda..', '1', 14, '7223372036854775806'),
+(24, 'Caneca Star Wars', 40, 'Item raro', '2', 10, '5223372036854775807');
 
 --
 -- Índices para tabelas despejadas
